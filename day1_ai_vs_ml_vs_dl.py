f75e486f51ddc47ef491bbecafb85d33fa138253
anuj7pradhan <86700815+anuj7pradhan@users.noreply.github.com>
2026-07-17 21:50:10 +0545

    Initial commit

---
+++ day1_ai_vs_ml_vs_dl.py
@@ -0,0 +1,82 @@
+"""
+Artificial Intelligence
+
+Machine Learning
+        -> Email spam detection
+        -> If mailko message contains somethinng phisy words(Lottery, Win, Free, Cash, etc) then it is spam mail
+            -> if yes
+                -> Mark as spam
+            -> if no
+                -> Mark as not spam
+
+                
+-> Machine learning
+            -> ML is a subset of AI 
+
+if(GharkoArea > 40 sq ft && bedroom == 2, kitchen == 3){
+gharkoPrice == 2000000 
+}
+
+* Machine Learning
+    -> Supervised Learning
+        -> definition: Supervised learning is a type of machine learning where the model is trained on labeled data, meaning that each training example is paired with an output label. 
+        -> The goal is for the model to learn the mapping from inputs to outputs so that it can make predictions on new, unseen data.
+            -> Example: -> Predicting house prices based on features like area, number of bedrooms, and number of kitchens.
+                        -> Hours of studies, passed or failed data are provided to the model to predict whether a student will pass or fail based on their study hours.
+    
+    -> Unsupervised Learning
+        -> definition: Unsupervised learning is a type of machine learning where the model is trained on unlabeled data, meaning that each training example is not paired with an output label. 
+        -> The goal is for the model to find hidden patterns or structures in the data.
+            -> Example: -> Clustering customers based on their purchasing behavior to identify different customer segments.
+                        -> Grouping students based on their study habits and performance without any prior knowledge of their grades.
+    
+            
+    -> Semi-Supervised Learning
+        -> definition: Semi-supervised learning is a type of machine learning that falls between supervised and unsupervised learning. It uses a combination of labeled and unlabeled data to train the model.
+            -> Example: -> Using a small amount of labeled data along with a large amount of unlabeled data to improve the performance of a model for image classification.
+                        -> Using a small set of labeled emails (spam or not spam) along with a large set of unlabeled emails to improve the accuracy of a spam detection model.
+            
+    -> Reinforcement Learning
+        -> definition: Reinforcement learning is a type of machine learning where the model learns to make decisions by performing actions in an environment and receiving rewards or penalties based on those actions.
+            -> Example: -> Training a robot to navigate a maze by rewarding it for reaching the exit and penalizing it for hitting walls.
+                        -> Re gareko garei garne type ko, or hit and trial type ko, jasto ki game khelne, robot lai sikhaune, etc.
+        
+
+Supervised Learning                                
+    -> Linear Regression
+        -> Gharko price prediction garne (gharko area, bedroom, kitchen ko basis ma)
+        -> kharcha anusar salary ko prediction garne (dinn ko kharcha anusar, 900, 1000 coffee anusar salary ko prediction garne)
+        -> numerical data ko basis ma prediction garne
+        -> small numerical data ko basis ma prediction garne
+        -> daily expense ko basis ma salary ko prediction garne
+        -> gharko area, bedroom, kitchen ko basis ma gharko price prediction garne
+
+    -> Logistic Regression
+        -> Clasification garne (yes or no, true or false, 0 or 1 thammyaune)
+        -> email spam detection garne (email ma phishy words cha ki chaina bhanera)
+        -> fridge ma aapko khana cha ki chaina bhanera clasification garne
+        -> more over boolean data ko basis ma clasification garne
+
+    -> Decision Tree
+        -> koi bidyarthiko attendence chahi > 90 cha bhane yes, video progression > 80 cha bhane yes, internship oppourtunity invite garne bhane yo chahi decision tree
+    
+    -> Random Forest
+        -> Dherai ota decision trees, ani. tya bata best filter out garne
+        -> doctor "A, B, C, or out of randomly eutalai choose garne bidhi" nai random forest 
+
+    -> KNN (K-Nearest Neighbors)
+        -> similarity ko basema prediction garne
+        ->  
+
+    -> Deep Learning
+        -> Definition   
+        Machine learning ko ek level ko sakiyo bhane deep learning aucha                      
+                        
+                        
+
+    
+# Scope
+    -> 
+
+        
+        """     
